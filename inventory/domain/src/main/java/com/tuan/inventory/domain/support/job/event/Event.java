1556cbbf7d3709bb4308442e0fce4fccaf4b7d37
yushuqiang <henry.yuh@gmail.com>
2014-04-29 17:05:50 +0800

    删除冗余代码
    Signed-off-by: yushuqiang <henry.yuh@gmail.com>

--- inventory/domain/src/main/java/com/tuan/inventory/domain/support/job/event/Event.java
+++ inventory/domain/src/main/java/com/tuan/inventory/domain/support/job/event/Event.java
@@ -3,7 +3,6 @@ package com.tuan.inventory.domain.support.job.event;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import com.tuan.core.common.lang.TuanBaseDO;
-import com.tuan.inventory.domain.support.enu.EventType;
 
 /**
  * 消息事件实体类
@@ -16,21 +15,11 @@ public class Event extends TuanBaseDO {
 
 	private Object data;//消息本体
 	
-	private EventType eventCode;//消息业务码
-	
 	private AtomicInteger tryCount= new AtomicInteger(0);//消息重试次数
 	
 	private long waitTime = 0;// 消息等待发送时间
 	
 	private String UUID;   //消息标识
-
-	public EventType getEventType() {
-		return eventCode;
-	}
-
-	public void setEventType(EventType eventCode) {
-		this.eventCode = eventCode;
-	}
 
 	public AtomicInteger getTryCount() {
 		return tryCount;
@@ -65,7 +54,6 @@ public class Event extends TuanBaseDO {
 
 	public String eventString(){
 	    StringBuffer eventStr=new StringBuffer("centerevent =[");
-	    eventStr.append("eventCode="+eventCode+";");
 	    eventStr.append("tryCount="+tryCount+";");
 	    eventStr.append("data="+data==null?"":data.toString()+";");
 	    eventStr.append("waitTime="+waitTime+";");
