394179f8d901a5620484f3e41e6b37cd48feb276
zhangbo <zhangbo8@55tuan.com>
2014-05-30 15:43:40 +0800

    修改反写启用事务

--- inventory/service.impl/src/main/java/com/tuan/inventory/service/impl/InventoryUpdateCallBackServiceImpl.java
+++ inventory/service.impl/src/main/java/com/tuan/inventory/service/impl/InventoryUpdateCallBackServiceImpl.java
@@ -1,6 +1,6 @@
 package com.tuan.inventory.service.impl;
+
 import java.lang.reflect.Type;
-import java.util.List;
 
 import javax.annotation.Resource;
 
@@ -10,77 +10,64 @@ import org.apache.commons.logging.LogFactory;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
-import com.tuan.inventory.dao.data.GoodsUpdateNumberDO;
+import com.tuan.core.common.service.TuanCallbackResult;
+import com.tuan.core.common.service.TuanServiceCallback;
+import com.tuan.core.common.service.TuanServiceTemplateImpl;
+import com.tuan.inventory.domain.InventoryUpdateByNotifyMessageDomain;
 import com.tuan.inventory.domain.repository.GoodUpdateNumberDomainRepository;
 import com.tuan.inventory.domain.support.logs.LogModel;
 import com.tuan.inventory.model.param.InventoryNotifyMessageParam;
-import com.tuan.inventory.model.param.SelectionNotifyMessageParam;
-import com.tuan.inventory.model.param.SuppliersNotifyMessageParam;
 import com.tuan.notifyserver.core.cclient.ConsumerReceiver;
 import com.tuan.notifyserver.core.connect.net.pojo.Message;
 
-public class InventoryUpdateCallBackServiceImpl extends AbstractService implements ConsumerReceiver {
+public class InventoryUpdateCallBackServiceImpl extends TuanServiceTemplateImpl
+		implements ConsumerReceiver {
 	@Resource
 	private GoodUpdateNumberDomainRepository goodUpdateNumberDomainRepository;
-	private final Log log=LogFactory.getLog("COMPENSATION.NOTIFY.LOG");
+	private final Log log = LogFactory.getLog("COMPENSATION.NOTIFY.LOG");
 	private final String method = "InventoryUpdateCallBackServiceImpl.receive";
+
 	@Override
 	public boolean receive(Message message) {
 		final LogModel lm = LogModel.newLogModel();
-		if(null == message){
-			log.info(lm.setMethod(method).addMetaData("resultCode", "参数无效").toJson());
+		if (null == message) {
+			log.info(lm.setMethod(method).addMetaData("resultCode", "参数无效")
+					.toJson());
 			return false;
 		}
-		log.info(lm.setMethod(method).addMetaData("message", message.getContent()).toJson());
-		InventoryNotifyMessageParam inventoryNotifyMessageParam=null;
+		log.info(lm.setMethod(method)
+				.addMetaData("message", message.getContent()).toJson());
+		InventoryNotifyMessageParam inventoryNotifyMessageParam = null;
 		try {
-			Type paramType = new TypeToken<InventoryNotifyMessageParam>(){}.getType();
-			inventoryNotifyMessageParam = new Gson().fromJson(message.getContent(),
-					paramType);
+			Type paramType = new TypeToken<InventoryNotifyMessageParam>() {
+			}.getType();
+			inventoryNotifyMessageParam = new Gson().fromJson(
+					message.getContent(), paramType);
 		} catch (JsonSyntaxException e) {
-			log.info(lm.setMethod(method).addMetaData("resultCode", "数据格式错误").toJson());
+			log.info(lm.setMethod(method).addMetaData("resultCode", "数据格式错误")
+					.toJson());
 			return false;
 		}
-		//库存调整
-		int totalNumber = inventoryNotifyMessageParam.getTotalNumber();
-		int leftNumber = inventoryNotifyMessageParam.getLeftNumber();
-		GoodsUpdateNumberDO goodsUpdateNumberDO=new GoodsUpdateNumberDO();
-		goodsUpdateNumberDO.setLeftNum(leftNumber);
-		goodsUpdateNumberDO.setTotalNum(totalNumber);
-		goodsUpdateNumberDO.setId(inventoryNotifyMessageParam.getGoodsId());
-		goodUpdateNumberDomainRepository.updateGoodsAttributesNumber(goodsUpdateNumberDO);
-		log.info(lm.setMethod(method).addMetaData("updatetraget", "GoodsAttributes").toJson());
-		List<SelectionNotifyMessageParam> selectionRelation =inventoryNotifyMessageParam.getSelectionRelation();
-		if(null!=selectionRelation){
-			for (SelectionNotifyMessageParam selectionNotifyMessageParam : selectionRelation) {
-				goodsUpdateNumberDO.setLeftNum(selectionNotifyMessageParam.getLeftNumber());
-				goodsUpdateNumberDO.setTotalNum(selectionNotifyMessageParam.getTotalNumber());
-				goodsUpdateNumberDO.setId(selectionNotifyMessageParam.getId());
-				goodUpdateNumberDomainRepository.updateSelectionRelationNumber(goodsUpdateNumberDO);
-				String wmsGoodsId=selectionNotifyMessageParam.getWmsGoodsId();
-				if(null!=wmsGoodsId){
-					Long wmsId=Long.parseLong(wmsGoodsId);
-					if(wmsId>0){
-					goodsUpdateNumberDO.setId(wmsId);
-					goodUpdateNumberDomainRepository.updataGoodsWmsNumByID(goodsUpdateNumberDO);
+		final InventoryUpdateByNotifyMessageDomain inventoryUpdateByNotifyMessageDomain = new InventoryUpdateByNotifyMessageDomain(
+				inventoryNotifyMessageParam, lm);
+		inventoryUpdateByNotifyMessageDomain
+				.setGoodUpdateNumberDomainRepository(goodUpdateNumberDomainRepository);
+
+		TuanCallbackResult callBackResult = super.execute(
+				new TuanServiceCallback() {
+					public TuanCallbackResult executeAction() {
+						return inventoryUpdateByNotifyMessageDomain.reWrite();
 					}
-				}
-			}
-			log.info(lm.setMethod(method).addMetaData("updatetraget", "SelectionRelation")
-					.addMetaData("size",selectionRelation.size()).toJson());
+
+					public TuanCallbackResult executeCheck() {
+						return TuanCallbackResult.success();
+					}
+				}, null);
+		final int resultCode = callBackResult.getResultCode();
+		if (resultCode == 0) {
+			return true;
 		}
-		List<SuppliersNotifyMessageParam> suppliersRelation =inventoryNotifyMessageParam.getSuppliersRelation();
-		if(null!=suppliersRelation){
-			for (SuppliersNotifyMessageParam suppliersNotifyMessageParam : suppliersRelation) {
-				goodsUpdateNumberDO.setLeftNum(suppliersNotifyMessageParam.getLeftNumber());
-				goodsUpdateNumberDO.setTotalNum(suppliersNotifyMessageParam.getTotalNumber());
-				goodsUpdateNumberDO.setId(suppliersNotifyMessageParam.getId());
-				goodUpdateNumberDomainRepository.updateSuppliersInventoryNumber(goodsUpdateNumberDO);
-			}
-			log.info(lm.setMethod(method).addMetaData("updatetraget", "Suppliers")
-					.addMetaData("size",suppliersRelation.size()).toJson());
-		}
-		return true;
+		return false;
 	}
 
 	public GoodUpdateNumberDomainRepository getGoodUpdateNumberDomainRepository() {
@@ -91,6 +78,4 @@ public class InventoryUpdateCallBackServiceImpl extends AbstractService implemen
 			GoodUpdateNumberDomainRepository goodUpdateNumberDomainRepository) {
 		this.goodUpdateNumberDomainRepository = goodUpdateNumberDomainRepository;
 	}
-	
-	
 }
