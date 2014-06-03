36d926aeb0e2119169cfc8eba58d4e37631a46bc
yushuqiang <henry.yuh@gmail.com>
2014-06-03 08:49:34 +0800

    import导入修改
    Signed-off-by: yushuqiang <henry.yuh@gmail.com>

--- inventory/domain/src/main/java/com/tuan/inventory/domain/InventoryUpdateByNotifyMessageDomain.java
+++ inventory/domain/src/main/java/com/tuan/inventory/domain/InventoryUpdateByNotifyMessageDomain.java
@@ -2,10 +2,10 @@ package com.tuan.inventory.domain;
 
 import java.util.List;
 
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.util.CollectionUtils;
 
 import com.tuan.core.common.service.TuanCallbackResult;
 import com.tuan.inventory.dao.data.GoodsUpdateNumberDO;
