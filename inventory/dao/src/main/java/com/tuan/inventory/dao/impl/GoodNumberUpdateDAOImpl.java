20b302c25d7f0f306b5e7521b3f60a9eb30bef0b
zhangbo <zhangbo8@55tuan.com>
2014-06-03 14:07:01 +0800

    dao优化

--- inventory/dao/src/main/java/com/tuan/inventory/dao/impl/GoodNumberUpdateDAOImpl.java
+++ inventory/dao/src/main/java/com/tuan/inventory/dao/impl/GoodNumberUpdateDAOImpl.java
@@ -14,30 +14,30 @@ import com.tuan.inventory.dao.data.GoodsUpdateNumberDO;
 public class GoodNumberUpdateDAOImpl extends SqlMapClientDaoSupport implements GoodNumUpdateDAO {
 	@Override
 	public void updateGoodsAttributesNumber(GoodsUpdateNumberDO goodsUpdateNumberDO) {
-		getSqlMapClientTemplate().update("set_sql_model_user");
+//		getSqlMapClientTemplate().update("set_sql_model_user");
 		getSqlMapClientTemplate().update("updateGoodsAttributesNumberByGID", goodsUpdateNumberDO);
-		getSqlMapClientTemplate().update("set_sql_model_sys");
+//		getSqlMapClientTemplate().update("set_sql_model_sys");
 		
 	}
 	@Override
 	public void updateSelectionRelationNumber(
 			GoodsUpdateNumberDO goodsUpdateNumberDO) {
-			getSqlMapClientTemplate().update("set_sql_model_user");
+//			getSqlMapClientTemplate().update("set_sql_model_user");
 			getSqlMapClientTemplate().update("updateSelectionRelationNumberBySID", goodsUpdateNumberDO);
-			getSqlMapClientTemplate().update("set_sql_model_sys");
+//			getSqlMapClientTemplate().update("set_sql_model_sys");
 	}
 	@Override
 	public void updateSuppliersInventoryNumber(
 			GoodsUpdateNumberDO goodsUpdateNumberDO) {
-		getSqlMapClientTemplate().update("set_sql_model_user");
+//		getSqlMapClientTemplate().update("set_sql_model_user");
 		getSqlMapClientTemplate().update("updateSuppliersInventoryNumberBySiID", goodsUpdateNumberDO);	
-		getSqlMapClientTemplate().update("set_sql_model_sys");
+//		getSqlMapClientTemplate().update("set_sql_model_sys");
 	}	
 
 	public void updataGoodsWmsNumByID(GoodsUpdateNumberDO goodsUpdateNumberDO) {
-		getSqlMapClientTemplate().update("set_sql_model_user");
+//		getSqlMapClientTemplate().update("set_sql_model_user");
 		getSqlMapClientTemplate().update("updataGoodsWmsNumByID", goodsUpdateNumberDO);
-		getSqlMapClientTemplate().update("set_sql_model_sys");
+//		getSqlMapClientTemplate().update("set_sql_model_sys");
 	}
 	
 }
