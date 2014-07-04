5f8bd0d0af0fa4031ec09f6f0b1ba3c98f1ede72
zhangbo <zhangbo8@55tuan.com>
2014-07-04 13:08:10 +0800

    master分支hessian服务

--- inventory/client/src/main/java/com/tuan/inventory/client/InventoryCenterFacade.java
+++ inventory/client/src/main/java/com/tuan/inventory/client/InventoryCenterFacade.java
@@ -1,10 +1,228 @@
 package com.tuan.inventory.client;
 
+import java.util.List;
+
+import com.tuan.inventory.model.GoodsBaseModel;
+import com.tuan.inventory.model.GoodsInventoryModel;
+import com.tuan.inventory.model.GoodsSelectionModel;
+import com.tuan.inventory.model.GoodsSuppliersModel;
+import com.tuan.inventory.model.WmsIsBeDeliveryModel;
+import com.tuan.inventory.model.param.AdjustInventoryParam;
+import com.tuan.inventory.model.param.AdjustWaterfloodParam;
+import com.tuan.inventory.model.param.CallbackParam;
+import com.tuan.inventory.model.param.CreaterInventoryParam;
+import com.tuan.inventory.model.param.InventoryScheduledParam;
+import com.tuan.inventory.model.param.OverrideAdjustInventoryParam;
+import com.tuan.inventory.model.param.UpdateInventoryParam;
+import com.tuan.inventory.model.param.UpdateWmsDataParam;
+import com.tuan.inventory.model.param.WmsInventoryParam;
 import com.tuan.inventory.model.result.CallResult;
-import com.tuan.inventory.model.result.InventoryQueryResult;
+import com.tuan.inventory.model.result.InventoryCallResult;
+import com.wowotrace.trace.model.Message;
 
 public interface InventoryCenterFacade {
-
-	public CallResult<InventoryQueryResult> querySelectionRelation(final String clientIp,
-			final String clientName,final long goodsId,final long SelectionRelationId);
+	
+	/**
+	 * 根据选型id获取选型库存信息
+	 * @param clientIp
+	 * @param clientName
+	 * @param userId
+	 * @param selectionId
+	 * @return
+	 */
+	public CallResult<GoodsSelectionModel> queryGoodsSelection(final String clientIp, final String clientName, final long goodsId,
+			final long selectionId);
+	/**
+	 * 根据商品分店id获取分店库存
+	 * @param clientIp
+	 * @param clientName
+	 * @param userId
+	 * @param suppliersId
+	 * @return
+	 */
+	public CallResult<GoodsSuppliersModel> queryGoodsSuppliers(final String clientIp, final String clientName, final long goodsId,
+			final long suppliersId);
+	/**
+	 * 根据商品id获取商品库存信息，
+	 * 包含(若有的话)选型的库存及分店的库存
+	 * @param clientIp
+	 * @param clientName
+	 * @param userId
+	 * @param goodsId
+	 * @return
+	 */
+	public CallResult<GoodsInventoryModel> queryGoodsInventory(final String clientIp, final String clientName, 
+			final long goodsId);
+	/**
+	 * 根据商品id获取商品选型库存列表
+	 * @param clientIp
+	 * @param clientName
+	 * @param userId
+	 * @param goodsId
+	 * @return
+	 */
+	public CallResult<List<GoodsSelectionModel>> queryGoodsSelectionList(final String clientIp, final String clientName, 
+			final long goodsId);
+	/**
+	 * 根据选型id 列表获取选型列表
+	 * @param clientIp
+	 * @param clientName
+	 * @param goodsId
+	 * @param selectionIdList
+	 * @return
+	 */
+	public CallResult<List<GoodsSelectionModel>> queryGoodsSelectionListBySelectionIdList(final String clientIp, final String clientName, 
+			final long goodsId,final List<Long> selectionIdList);
+	/**
+	 * 根据商品id获取商品分店库存列表
+	 * @param clientIp
+	 * @param clientName
+	 * @param userId
+	 * @param goodsId
+	 * @return
+	 */
+	public CallResult<List<GoodsSuppliersModel>> queryGoodsSuppliersListByGoodsId(final String clientIp, final String clientName, 
+			final long goodsId);
+	/**
+	 * 根据分店id list获取分店列表
+	 * @param clientIp
+	 * @param clientName
+	 * @param goodsId
+	 * @param suppliersIdList
+	 * @return
+	 */
+	public CallResult<List<GoodsSuppliersModel>> queryGoodsSuppliersListBySuppliersIdList(final String clientIp, final String clientName, 
+			final long goodsId,final List<Long> suppliersIdList);
+	/***
+	 * 根据物流码查询发货仓库信息
+	 * @param clientIp
+	 * @param clientName
+	 * @param goodsId
+	 * @return
+	 */
+	public CallResult<WmsIsBeDeliveryModel> queryWmsIsBeDeliveryByWmsGoodsId(final String clientIp, final String clientName, 
+			final String wmsGoodsId,final String isBeDelivery);
+	/**
+	 * 根据商品基本id获取销量信息
+	 * @param clientIp
+	 * @param clientName
+	 * @param goodsBaseId
+	 * @return
+	 */
+	public CallResult<GoodsBaseModel> querySalesCountByGoodsBaseId(final String clientIp, final String clientName, 
+			final String goodsBaseId);
+	/**
+	 * 回调确认过的队列的消费接口
+	 * @param clientIp
+	 * @param clientName
+	 * @param traceMessage
+	 */
+	public void confirmQueueConsume(String clientIp,
+			String clientName,Message traceMessage);
+	/**
+	 * 被锁定队列的消费接口
+	 * @param clientIp
+	 * @param clientName
+	 * @param param
+	 * @param traceMessage
+	 */
+	public void lockedQueueConsume(String clientIp,
+			String clientName,InventoryScheduledParam param,Message traceMessage);
+	/**
+	 * 日志队列的消费接口
+	 * @param clientIp
+	 * @param clientName
+	 * @param traceMessage
+	 */
+	public void logsQueueConsume(String clientIp,
+			String clientName,Message traceMessage);
+	/** 
+	 * 创建库存接口，根据传入的参数创建库存
+	 * @param clientIp String 客户端ip地址
+	 * @param clientName String 客户端名称
+	 * @param param UpdateInventoryParam 创建库存所需参数类对象
+	 * @return InventoryCallResult 接口调用结果对象
+	 */
+	public InventoryCallResult createInventory(final String clientIp, final String clientName
+			,final CreaterInventoryParam param,Message traceMessage);
+	/**
+	 * 更新商品库存
+	 * @param clientIp
+	 * @param clientName
+	 * @param param
+	 * @param traceMessage
+	 * @return
+	 */
+	public InventoryCallResult updateInventory(
+			final String clientIp, final String clientName,final UpdateInventoryParam param,Message traceMessage);
+	/***
+	 * 库存扣减完成后，回调确认接口
+	 * @param clientIp
+	 * @param clientName
+	 * @param param
+	 * @param traceMessage
+	 * @return
+	 */
+	public InventoryCallResult callbackAckInventory(
+			final String clientIp, final String clientName,final CallbackParam param,Message traceMessage);
+	/**
+	 * 手工调整库存
+	 * @param clientIp
+	 * @param clientName
+	 * @param param
+	 * @param traceMessage
+	 * @return
+	 */
+	public InventoryCallResult adjustmentInventory(
+			final String clientIp, final String clientName,final AdjustInventoryParam param,Message traceMessage);
+	/**
+	 * 手工调整注水值
+	 * @param clientIp
+	 * @param clientName
+	 * @param param
+	 * @param traceMessage
+	 * @return
+	 */
+	public InventoryCallResult adjustmentWaterflood(
+			final String clientIp, final String clientName,final AdjustWaterfloodParam param,Message traceMessage);
+	/**
+	 * 创建物流库存接口,根据传入的参数创建物流相关库存
+	 * @param clientIp
+	 * @param clientName
+	 * @param param
+	 * @param traceMessage
+	 * @return
+	 */
+	public InventoryCallResult createWmsInventory(final String clientIp, final String clientName
+			,final WmsInventoryParam param,Message traceMessage);
+	/**
+	 * 调整物流库存：增量调整
+	 * @param clientIp
+	 * @param clientName
+	 * @param param
+	 * @param traceMessage
+	 * @return
+	 */
+	public InventoryCallResult adjustWmsInventory(
+			final String clientIp, final String clientName,final WmsInventoryParam param,Message traceMessage);
+	/**
+	 * 库存调整：全量调整
+	 * @param clientIp
+	 * @param clientName
+	 * @param param
+	 * @param traceMessage
+	 * @return
+	 */
+	public InventoryCallResult overrideAdjustInventory(
+			final String clientIp, final String clientName,final OverrideAdjustInventoryParam param,Message traceMessage);
+	/**
+	 * 物流关系数据更新接口
+	 * @param clientIp
+	 * @param clientName
+	 * @param param
+	 * @param traceMessage
+	 * @return
+	 */
+	public InventoryCallResult updateWmsData(
+			final String clientIp, final String clientName,final UpdateWmsDataParam param,Message traceMessage);
 }
--- inventory/client/src/main/java/com/tuan/inventory/client/impl/InventoryCenterFacadeImpl.java
+++ inventory/client/src/main/java/com/tuan/inventory/client/impl/InventoryCenterFacadeImpl.java
@@ -1,17 +1,162 @@
 package com.tuan.inventory.client.impl;
 
+import java.util.List;
+
 import com.tuan.inventory.client.InventoryCenterFacade;
+import com.tuan.inventory.model.GoodsBaseModel;
+import com.tuan.inventory.model.GoodsInventoryModel;
+import com.tuan.inventory.model.GoodsSelectionModel;
+import com.tuan.inventory.model.GoodsSuppliersModel;
+import com.tuan.inventory.model.WmsIsBeDeliveryModel;
+import com.tuan.inventory.model.param.AdjustInventoryParam;
+import com.tuan.inventory.model.param.AdjustWaterfloodParam;
+import com.tuan.inventory.model.param.CallbackParam;
+import com.tuan.inventory.model.param.CreaterInventoryParam;
+import com.tuan.inventory.model.param.InventoryScheduledParam;
+import com.tuan.inventory.model.param.OverrideAdjustInventoryParam;
+import com.tuan.inventory.model.param.UpdateInventoryParam;
+import com.tuan.inventory.model.param.UpdateWmsDataParam;
+import com.tuan.inventory.model.param.WmsInventoryParam;
 import com.tuan.inventory.model.result.CallResult;
-import com.tuan.inventory.model.result.InventoryQueryResult;
-import com.tuan.inventory.service.GoodSeletionQueryService;
+import com.tuan.inventory.model.result.InventoryCallResult;
+import com.tuan.inventory.service.GoodsInventoryQueryService;
+import com.tuan.inventory.service.GoodsInventoryScheduledService;
+import com.tuan.inventory.service.GoodsInventoryUpdateService;
+import com.wowotrace.trace.model.Message;
 
 public class InventoryCenterFacadeImpl implements InventoryCenterFacade {
-
-	private GoodSeletionQueryService goodSeletionQueryService;
-
+	private GoodsInventoryQueryService  goodsInventoryQuery;
+	private GoodsInventoryUpdateService goodsInventoryUpdate;
+	private GoodsInventoryScheduledService goodsInventoryScheduled;
 	@Override
-	public CallResult<InventoryQueryResult> querySelectionRelation(final String clientIp,
-			final String clientName,final long goodsId,final long SelectionRelationId) {
-		return goodSeletionQueryService.querySelectionRelation(clientIp, clientName, goodsId, SelectionRelationId);
+	public CallResult<GoodsSelectionModel> queryGoodsSelection(String clientIp,
+			String clientName, long goodsId, long selectionId) {
+		return goodsInventoryQuery.findGoodsSelectionBySelectionId(clientIp, clientName, goodsId, selectionId);
 	}
+	@Override
+	public CallResult<GoodsSuppliersModel> queryGoodsSuppliers(String clientIp,
+			String clientName, long goodsId, long suppliersId) {
+		return goodsInventoryQuery.findGoodsSuppliersBySuppliersId(clientIp, clientName, goodsId, suppliersId);
+	}
+	@Override
+	public CallResult<GoodsInventoryModel> queryGoodsInventory(String clientIp,
+			String clientName, long goodsId) {
+		return goodsInventoryQuery.findGoodsInventoryByGoodsId(clientIp, clientName, goodsId);
+	}
+	@Override
+	public CallResult<List<GoodsSelectionModel>> queryGoodsSelectionList(
+			String clientIp, String clientName, long goodsId) {
+		return goodsInventoryQuery.findGoodsSelectionListByGoodsId(clientIp, clientName, goodsId);
+	}
+	@Override
+	public CallResult<List<GoodsSelectionModel>> queryGoodsSelectionListBySelectionIdList(
+			String clientIp, String clientName, long goodsId,
+			List<Long> selectionIdList) {
+		return goodsInventoryQuery.findGoodsSelectionListBySelectionIdList(clientIp, clientName, goodsId, selectionIdList);
+	}
+	@Override
+	public CallResult<List<GoodsSuppliersModel>> queryGoodsSuppliersListByGoodsId(
+			String clientIp, String clientName, long goodsId) {
+		return goodsInventoryQuery.findGoodsSuppliersListByGoodsId(clientIp, clientName, goodsId);
+	}
+	@Override
+	public CallResult<List<GoodsSuppliersModel>> queryGoodsSuppliersListBySuppliersIdList(
+			String clientIp, String clientName, long goodsId,
+			List<Long> suppliersIdList) {
+		return goodsInventoryQuery.findGoodsSuppliersListBySuppliersIdList(clientIp, clientName, goodsId, suppliersIdList);
+	}
+	@Override
+	public CallResult<WmsIsBeDeliveryModel> queryWmsIsBeDeliveryByWmsGoodsId(
+			String clientIp, String clientName, String wmsGoodsId,
+			String isBeDelivery) {
+		return goodsInventoryQuery.findWmsIsBeDeliveryByWmsGoodsId(clientIp, clientName, wmsGoodsId, isBeDelivery);
+	}
+	@Override
+	public CallResult<GoodsBaseModel> querySalesCountByGoodsBaseId(
+			String clientIp, String clientName, String goodsBaseId) {
+		return goodsInventoryQuery.findSalesCountByGoodsBaseId(clientIp, clientName, goodsBaseId);
+	}
+	@Override
+	public void confirmQueueConsume(String clientIp, String clientName,
+			Message traceMessage) {
+		goodsInventoryScheduled.confirmQueueConsume(clientIp, clientName, traceMessage);
+	}
+	@Override
+	public void lockedQueueConsume(String clientIp, String clientName,
+			InventoryScheduledParam param, Message traceMessage) {
+		goodsInventoryScheduled.lockedQueueConsume(clientIp, clientName, param, traceMessage);		
+	}
+	@Override
+	public void logsQueueConsume(String clientIp, String clientName,
+			Message traceMessage) {
+		goodsInventoryScheduled.logsQueueConsume(clientIp, clientName, traceMessage)	;	
+	}
+	@Override
+	public InventoryCallResult createInventory(String clientIp,
+			String clientName, CreaterInventoryParam param, Message traceMessage) {
+		return goodsInventoryUpdate.createInventory(clientIp, clientName, param, traceMessage);
+	}
+	@Override
+	public InventoryCallResult updateInventory(String clientIp,
+			String clientName, UpdateInventoryParam param, Message traceMessage) {
+		return goodsInventoryUpdate.updateInventory(clientIp, clientName, param, traceMessage);
+	}
+	@Override
+	public InventoryCallResult callbackAckInventory(String clientIp,
+			String clientName, CallbackParam param, Message traceMessage) {
+		return goodsInventoryUpdate.callbackAckInventory(clientIp, clientName, param, traceMessage);
+	}
+	@Override
+	public InventoryCallResult adjustmentInventory(String clientIp,
+			String clientName, AdjustInventoryParam param, Message traceMessage) {
+		return goodsInventoryUpdate.adjustmentInventory(clientIp, clientName, param, traceMessage);
+	}
+	@Override
+	public InventoryCallResult adjustmentWaterflood(String clientIp,
+			String clientName, AdjustWaterfloodParam param, Message traceMessage) {
+		return goodsInventoryUpdate.adjustmentWaterflood(clientIp, clientName, param, traceMessage);
+	}
+	@Override
+	public InventoryCallResult createWmsInventory(String clientIp,
+			String clientName, WmsInventoryParam param, Message traceMessage) {
+		return goodsInventoryUpdate.createWmsInventory(clientIp, clientName, param, traceMessage);
+	}
+	@Override
+	public InventoryCallResult adjustWmsInventory(String clientIp,
+			String clientName, WmsInventoryParam param, Message traceMessage) {
+		return goodsInventoryUpdate.adjustWmsInventory(clientIp, clientName, param, traceMessage);
+	}
+	@Override
+	public InventoryCallResult overrideAdjustInventory(String clientIp,
+			String clientName, OverrideAdjustInventoryParam param,
+			Message traceMessage) {
+		return goodsInventoryUpdate.overrideAdjustInventory(clientIp, clientName, param, traceMessage);
+	}
+	@Override
+	public InventoryCallResult updateWmsData(String clientIp,
+			String clientName, UpdateWmsDataParam param, Message traceMessage) {
+		return goodsInventoryUpdate.updateWmsData(clientIp, clientName, param, traceMessage);
+	}
+	public GoodsInventoryQueryService getGoodsInventoryQuery() {
+		return goodsInventoryQuery;
+	}
+	public void setGoodsInventoryQuery(
+			GoodsInventoryQueryService goodsInventoryQuery) {
+		this.goodsInventoryQuery = goodsInventoryQuery;
+	}
+	public GoodsInventoryUpdateService getGoodsInventoryUpdate() {
+		return goodsInventoryUpdate;
+	}
+	public void setGoodsInventoryUpdate(
+			GoodsInventoryUpdateService goodsInventoryUpdate) {
+		this.goodsInventoryUpdate = goodsInventoryUpdate;
+	}
+	public GoodsInventoryScheduledService getGoodsInventoryScheduled() {
+		return goodsInventoryScheduled;
+	}
+	public void setGoodsInventoryScheduled(
+			GoodsInventoryScheduledService goodsInventoryScheduled) {
+		this.goodsInventoryScheduled = goodsInventoryScheduled;
+	}
+
 }
