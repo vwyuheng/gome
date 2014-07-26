ba44f4ddf2a4d2939a9e226fddc5df89c204aa53
yushuqiang <henry.yuh@gmail.com>
2014-07-26 12:24:34 +0800

    日志处理
    Signed-off-by: yushuqiang <henry.yuh@gmail.com>

--- inventory/domain/src/main/java/com/tuan/inventory/domain/InventoryLogsScheduledDomain.java
+++ inventory/domain/src/main/java/com/tuan/inventory/domain/InventoryLogsScheduledDomain.java
@@ -40,25 +40,23 @@ public class InventoryLogsScheduledDomain extends AbstractDomain {
 
 		try {
 			// 业务检查前的预处理
+
 			int movedTotalCount = 0;
 			final Long currentMaxId = goodsInventoryDomainRepository.queryLogQueueMaxLenth(QueueConstant.QUEUE_LOGS_MESSAGE);
 			if (currentMaxId == null || currentMaxId.longValue() <= 0) {
 				// 表中无记录，直接返回
-				logLogs.warn("日志队列获取当前最大记录lenth(数)返回空，结束本次日志队列历史记录迁移");
+				logLogs.info("日志队列获取当前最大记录lenth(数)返回空，结束本次日志队列历史记录迁移");
 				return CreateInventoryResultEnum.SUCCESS;
 			}
 			while (true) {
 				
 				List<GoodsInventoryActionModel> queueLogList =  goodsInventoryDomainRepository.queryFirstInGoodsInventoryAction();
 				if(CollectionUtils.isEmpty(queueLogList)) {
-					logLogs.warn("日志队列" + queueLogList + "无待迁移的日志队列记录！");
+					logLogs.info("日志队列" + queueLogList + "无待迁移的日志队列记录！");
 					break;
 				}
 				 long startTime = System.currentTimeMillis();
-				 String method = "logsEventHandle.handleEvent";
-				final LogModel lm = LogModel.newLogModel(method);
-				logLogs.warn(lm.setMethod(method).addMetaData("batch handler logs start", startTime)
-							.toJson(true));
+				 logLogs.info("logsEventHandle.handleEvent batch handler logs start:" +startTime);
 				int curMovedCount = queueLogList.size();
 				if (curMovedCount > 0) {
 					if (fillActionEvent(queueLogList)) {	 //从队列中取事件
@@ -66,14 +64,14 @@ public class InventoryLogsScheduledDomain extends AbstractDomain {
 							boolean eventResult = logsEventHandle.handleEvent(event);
 							
 							if(eventResult) {  //落mysql成功的话,也就是消费日志消息成功
-								for (GoodsInventoryActionModel model : queueLogList) {
-									String remmethod = "lremLogQueue,[移除日志队列]:("+model+",eventResult:)"+eventResult;
-									final LogModel remlm = LogModel.newLogModel(remmethod);
+								for (GoodsInventoryActionModel model : queueLogList) {  //目的就是循环500次移除前500个元素
+									//String remmethod = "lremLogQueue,[移除日志队列]:("+model+",eventResult:)"+eventResult;
 									//循环删除所有元素
 									//Long rem = this.goodsInventoryDomainRepository.lremLogQueue(model);
 									String bydelLogMember = this.goodsInventoryDomainRepository.lpop(QueueConstant.QUEUE_LOGS_MESSAGE);
 									
-									logLogs.warn(remlm.setMethod(remmethod).addMetaData("日志id", model!=null?model.getId():0).addMetaData("删除日志", bydelLogMember));
+									//logLogs.info("lremLogQueue,[移除日志队列]日志id:"+ model!=null?model.getId():0+",删除日志详细信息："+bydelLogMember);
+									logLogs.info("从redis中移除的日志:"+bydelLogMember);
 								}
 								
 							   }
@@ -82,10 +80,9 @@ public class InventoryLogsScheduledDomain extends AbstractDomain {
 				 
 				// 记录本次程序运行的处理结果
 				long endTime = System.currentTimeMillis();
-				String runResult = "[" + method + "]业务处理历时" + (startTime - endTime)
+				String runResult = "[批量处理日志]业务处理历时" + (startTime - endTime)
 						+ "milliseconds(毫秒)执行完成!,日志队列本次循环处理条数:"+curMovedCount;
-				logLogs.warn(lm.setMethod(method).addMetaData("endTime", endTime)
-						.addMetaData("runResult", runResult));
+				logLogs.info("endTime:"+endTime+",runResult:"+runResult);
 				movedTotalCount += curMovedCount;
 				if (curMovedCount < dealStep) {
 					break;
@@ -100,7 +97,7 @@ public class InventoryLogsScheduledDomain extends AbstractDomain {
 			}
 			
 			// 记录本次程序运行的处理结果
-			logLogs.warn("日志队列本次程序运行处理总条数＝" + movedTotalCount);
+			logLogs.info("日志队列本次程序运行处理总条数＝" + movedTotalCount);
 			
 			
 		} catch (Exception e) {
