74942b884fb493241946fe97334d32f9ad3ef8f7
yushuqiang <henry.yuh@gmail.com>
2014-07-26 13:48:40 +0800

    100-300毫秒
    Signed-off-by: yushuqiang <henry.yuh@gmail.com>

--- inventory/domain/src/main/java/com/tuan/inventory/domain/InventoryLogsScheduledDomain.java
+++ inventory/domain/src/main/java/com/tuan/inventory/domain/InventoryLogsScheduledDomain.java
@@ -88,7 +88,7 @@ public class InventoryLogsScheduledDomain extends AbstractDomain {
 					break;
 				}
 				try {
-					int rd = (int)(Math.random()*100)%3+1;//1~3秒
+					int rd = (int)(Math.random()*100)%3+1;//100~300毫秒
 					Thread.sleep(rd*haveRest);
 				} catch (Exception e) {
 					logLogs.debug(e.getMessage(), e);
