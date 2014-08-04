04c152fedf1d4b25b71aa41fe1d0f6bc755850e4
yushuqiang <henry.yuh@gmail.com>
2014-08-04 15:28:00 +0800

    创建库存时无需发送消息
    Signed-off-by: yushuqiang <henry.yuh@gmail.com>

--- inventory/service.impl/src/main/java/com/tuan/inventory/service/impl/GoodsInventoryUpdateServiceImpl.java
+++ inventory/service.impl/src/main/java/com/tuan/inventory/service/impl/GoodsInventoryUpdateServiceImpl.java
@@ -110,7 +110,7 @@ public class GoodsInventoryUpdateServiceImpl  extends AbstractInventoryService i
 
 			@Override
 			public void executeAfter() {
-				inventoryCreatorDomain.sendNotify();
+				//inventoryCreatorDomain.sendNotify();
 			}
 		});
 		long endTime = System.currentTimeMillis();
