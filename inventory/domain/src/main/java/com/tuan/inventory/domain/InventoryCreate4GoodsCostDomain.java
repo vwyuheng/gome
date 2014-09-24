9212c85ec55fbcbbe514d92b518087b1ef808c35
yushuqiang <henry.yuh@gmail.com>
2014-09-24 18:33:41 +0800

    增加改价debug日志
    Signed-off-by: yushuqiang <henry.yuh@gmail.com>

--- inventory/domain/src/main/java/com/tuan/inventory/domain/InventoryCreate4GoodsCostDomain.java
+++ inventory/domain/src/main/java/com/tuan/inventory/domain/InventoryCreate4GoodsCostDomain.java
@@ -169,6 +169,10 @@ public class InventoryCreate4GoodsCostDomain extends AbstractDomain {
 			long startTime = System.currentTimeMillis();
 			String method = "OrderQueryService.queryNupayOrderGoodsNum,preGoodsId:"+preGoodsId;
 			final LogModel lm = LogModel.newLogModel(method);
+			if(logupdate.isDebugEnabled()) {
+				logupdate.debug(lm.setMethod(method).addMetaData("start", startTime)
+						.toJson(false));
+			}
 			logupdate.info(lm.setMethod(method).addMetaData("start", startTime)
 					.toJson(false));
 			CallResult<OrderQueryResult>  cllResult= inventoryCenterExtFacade.queryNupayOrderGoodsNum( "INVENTORY_"+ClientNameEnum.INNER_SYSTEM.getValue(),"", preGoodsId);
@@ -183,6 +187,10 @@ public class InventoryCreate4GoodsCostDomain extends AbstractDomain {
 				long endTime = System.currentTimeMillis();
 				String runResult = "[" + method + "]业务处理历时" + (startTime - endTime)
 						+ "milliseconds(毫秒)执行完成!takeNum="+takeNum;
+				if(logupdate.isDebugEnabled()) {
+					logupdate.debug(lm.setMethod(method).addMetaData("endTime", endTime).addMetaData("preGoodsId", preGoodsId)
+							.addMetaData("runResult", runResult).addMetaData("message", result.getDescription()).toJson(false));
+				}
 				logupdate.info(lm.setMethod(method).addMetaData("endTime", endTime).addMetaData("preGoodsId", preGoodsId)
 						.addMetaData("runResult", runResult).addMetaData("message", result.getDescription()).toJson(false));
 			} else {
